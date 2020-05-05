1773febdb179856761ab44c9c3865120a0ac4deb
aung <aung@cybersprout.com>
2020-05-05 18:22:26 +0800

    no message

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// Version
+define('VERSION', '2.3.0.2');
+
+// DB connection 
+if (is_file('system/db_config.php')) { 
+	require_once('system/db_config.php');
+}
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
